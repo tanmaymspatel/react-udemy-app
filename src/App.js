cdb900757cbd355cea0b30859338a1d8b0c33782
Tanmay Patel <tanmay.1rivet@gmail.com>
2022-11-02 12:03:43 +0530

    Projrct Set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='h-100'>
+      Hello
+    </div>
+  );
+}
+
+export default App;
